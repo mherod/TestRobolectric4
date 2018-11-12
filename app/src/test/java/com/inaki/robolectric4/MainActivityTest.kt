473153fc3143482217af3a7cbe53b35f90d44374
Matthew Herod <matthew.herod@myunidays.com>
2018-11-12 14:20:55 +0000

    Use androidx.test.ext.junit.runners.AndroidJUnit4

--- app/src/test/java/com/inaki/robolectric4/MainActivityTest.kt
+++ app/src/test/java/com/inaki/robolectric4/MainActivityTest.kt
@@ -5,8 +5,8 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
